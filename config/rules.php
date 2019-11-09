d0dc73a50dc6e69bbf8b5f64f0f760b1fe28e959
NastuzziSamy <samy@nastuzzi.fr>
2019-11-09 15:05:09 +0100

    Add a provider and a config file for rules

---
+++ config/rules.php
@@ -0,0 +1,106 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Rule manager
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the class that will manage all rules.
+    |
+    */
+
+    'manager' => 'Laramore\\Elements\\RuleManager',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default rules
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the default rules used in fields.
+    |
+    */
+
+    'configurations' => [
+        'nullable' => [
+            'description' => 'Set the value as nullable by default',
+            'removes' => [
+                'not_nullable',
+            ],
+        ],
+        'not_nullable' => [
+            'description' => 'Forbid to be nullable',
+            'removes' => [
+                'nullable',
+            ],
+        ],
+        'visible' => [
+            'description' => 'Set as visible',
+        ],
+        'fillable' => [
+            'description' => 'Set as fillable',
+        ],
+        'required' => [
+            'description' => 'Require a value',
+        ],
+        'unsigned' => [
+            'description' => 'Force a value to be unsigned',
+        ],
+        'negative' => [
+            'description' => 'Force the value to be negative',
+            'adds' => [
+                'unsigned',
+            ],
+        ],
+        'require_sign' => [
+            'description' => 'Force the value to be of the right sign',
+        ],
+        'not_zero' => [
+            'description' => 'Forbid value zero',
+        ],
+        'not_blank' => [
+            'description' => 'Forbid blank value',
+        ],
+        'trim' => [
+            'description' => 'Trim value',
+        ],
+        'need_lowercase' => [
+            'description' => 'Need at least one lowercase caracter',
+        ],
+        'need_uppercase' => [
+            'description' => 'Need at least one uppercase caracter',
+        ],
+        'need_number' => [
+            'description' => 'Need at least one number caracter',
+        ],
+        'need_special' => [
+            'description' => 'Need at least one special caracter',
+        ],
+        'accept_username' => [
+            'description' => 'Accept to have only a username instead of a full definition (email)',
+        ],
+        'only_same_cdn' => [
+            'description' => 'Only accepts a range of cdn',
+        ],
+        'caracter_resize' => [
+            'description' => 'Cut the value at the decided length',
+        ],
+        'caracter_resize' => [
+            'description' => 'Cut the value at the decided length',
+        ],
+        'word_resize' => [
+            'description' => 'Cut the value at the decided length, without cutting a word',
+        ],
+        'sentence_resize' => [
+            'description' => 'Cut the value at the decided length, without cutting a sentence',
+        ],
+        'dots_on_resize' => [
+            'description' => 'Add dots if the value is cut',
+        ],
+        'use_current' => [
+            'description' => 'Use the current value',
+        ],
+    ],
+
+];
---
+++ src/Providers/RulesProvider.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Prepare type manager.
+ *
+ * @author Samy Nastuzzi <samy@nastuzzi.fr>
+ *
+ * @copyright Copyright (c) 2019
+ * @license MIT
+ */
+
+namespace Laramore\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laramore\Interfaces\IsALaramoreProvider;
+use Laramore\Traits\Providers\MergesConfig;
+
+class RulesProvider extends ServiceProvider implements IsALaramoreProvider
+{
+    use MergesConfig;
+
+    /**
+     * Rule manager.
+     *
+     * @var RuleManager
+     */
+    protected static $manager;
+
+    /**
+     * Register our facade and create the manager.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../../config/rules.php', 'rules',
+        );
+
+        $this->app->singleton('Rules', function() {
+            return static::getManager();
+        });
+
+        $this->app->booted([$this, 'bootedCallback']);
+    }
+
+    /**
+     * Publish the config linked to the manager.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../../config/rules.php' => config_path('rules.php'),
+        ]);
+    }
+
+    /**
+     * Return the default values for the manager of this provider.
+     *
+     * @return array
+     */
+    public static function getDefaults(): array
+    {
+        return config('rules.configurations');
+    }
+
+    /**
+     * Generate the corresponded manager.
+     *
+     * @return void
+     */
+    protected static function generateManager()
+    {
+        $class = config('rules.manager');
+
+        static::$manager = new $class();
+
+        static::$manager->define('adds', []);
+        static::$manager->define('removes', []);
+        static::$manager->set(static::getDefaults());
+    }
+
+    /**
+     * Return the generated manager for this provider.
+     *
+     * @return object
+     */
+    public static function getManager(): object
+    {
+        if (\is_null(static::$manager)) {
+            static::generateManager();
+        }
+
+        return static::$manager;
+    }
+
+    /**
+     * Lock all managers after booting.
+     *
+     * @return void
+     */
+    public function bootedCallback()
+    {
+        static::getManager()->lock();
+    }
+}
