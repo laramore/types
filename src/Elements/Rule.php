7755ac2cf7c12b28a2380f295f02facae356b076
NastuzziSamy <samy@nastuzzi.fr>
2019-12-01 21:40:24 +0100

    Remove useless config method and auto setters

--- src/Elements/Rule.php
+++ src/Elements/Rule.php
@@ -14,45 +14,4 @@ use Laramore\Facades\Rules;
 
 class Rule extends Element
 {
-    /**
-     * Define all rules to add if this rule is added.
-     *
-     * @param array $rules
-     * @return self
-     */
-    public function setAdds(array $rules)
-    {
-        $this->needsToBeUnlocked();
-
-        $this->values['adds'] = \array_map(function ($rule) {
-            if (\is_string($rule)) {
-                return Rules::has($rule) ? Rules::get($rule) : Rules::create($rule);
-            } else {
-                return $rule;
-            }
-        }, $rules);
-
-        return $this;
-    }
-
-    /**
-     * Define all rules to remove if this rule is added.
-     *
-     * @param array $rules
-     * @return self
-     */
-    public function setRemoves(array $rules)
-    {
-        $this->needsToBeUnlocked();
-
-        $this->values['removes'] = \array_map(function ($rule) {
-            if (\is_string($rule)) {
-                return Rules::has($rule) ? Rules::get($rule) : Rules::create($rule);
-            } else {
-                return $rule;
-            }
-        }, $rules);
-
-        return $this;
-    }
 }
--- src/Elements/Type.php
+++ src/Elements/Type.php
@@ -11,33 +11,9 @@
 namespace Laramore\Elements;
 
 use Laramore\Facades\Rules;
-use Laramore\Interfaces\IsConfigurable;
 
-class Type extends Element implements IsConfigurable
+class Type extends Element
 {
-    /**
-     * Return the configuration path for this field.
-     *
-     * @param string $path
-     * @return mixed
-     */
-    public function getConfigPath(string $path=null)
-    {
-        return 'types.configurations.'.$this->getName().(\is_null($path) ? '' : '.'.$path);
-    }
-
-    /**
-     * Return the configuration for this field.
-     *
-     * @param string $path
-     * @param mixed  $default
-     * @return mixed
-     */
-    public function getConfig(string $path=null, $default=null)
-    {
-        return config($this->getConfigPath($path), $default);
-    }
-
     /**
      * Tell if a value is of the right type.
      *
@@ -55,26 +31,5 @@ class Type extends Element implements IsConfigurable
         $type = "\is_$native";
 
         return $type($value);
-    }
-
-    /**
-     * Define all rules to add if this rule is added.
-     *
-     * @param array $rules
-     * @return self
-     */
-    public function setDefaultRules(array $rules)
-    {
-        $this->needsToBeUnlocked();
-
-        $this->values['default_rules'] = \array_map(function ($rule) {
-            if (\is_string($rule)) {
-                return Rules::has($rule) ? Rules::get($rule) : Rules::create($rule);
-            } else {
-                return $rule;
-            }
-        }, $rules);
-
-        return $this;
     }
 }
